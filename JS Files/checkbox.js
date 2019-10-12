ac842bd404566d26557dc4ace7d1ee1ad5d39430
ErikLoo <gdsyzx@hotmail.com>
2019-10-12 15:48:20 -0400

    asdf

--- JS Files/checkbox.js
+++ JS Files/checkbox.js
@@ -1,8 +1,25 @@
 function setupCheckbox(ElenName){
        //click event for checkboxes
+        //initialize the value
+        ElenName.find(".No").eq(0).prop("checked",false);
+        ElenName.find(".Yes").eq(0).prop("checked",false);
+
+        ElenName.find(".aria-widget-slider").eq(1).hide(); 
+        //the slider is by default hidden
+
         ElenName.find(".Yes").on("click",function(){
-            alert("Yes" +count)
+            // alert("Yes" +count)
             //uncheck No box
-            ElenName.find(".No").attr("checked",false)
+            ElenName.find(".No").eq(0).prop("checked",false);
+            //show the slider
+            ElenName.find(".aria-widget-slider").eq(1).show()
+        });
+
+        ElenName.find(".No").on("click",function(){
+            // alert("No" +count)
+            //uncheck No box
+            ElenName.find(".Yes").eq(0).prop("checked",false);
+            //hide the slider
+            ElenName.find(".aria-widget-slider").eq(1).hide()
         });
 }
