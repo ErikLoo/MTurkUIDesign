52eaf89971681a88ea58ae2debeb161255d66da2
ErikLoo <gdsyzx@hotmail.com>
2019-10-13 00:09:32 -0400

    1.check boxes fully working now.2.Revised tabs so that each tab has different colour

--- JS Files/checkbox.js
+++ JS Files/checkbox.js
@@ -1,10 +1,10 @@
-function setupCheckbox(ElenName){
+function setupCheckbox(ElenName,SliderName){
        //click event for checkboxes
         //initialize the value
         ElenName.find(".No").eq(0).prop("checked",false);
         ElenName.find(".Yes").eq(0).prop("checked",false);
 
-        ElenName.find(".aria-widget-slider").eq(1).hide(); 
+        SliderName.find(".aria-widget-slider").eq(1).hide(); 
         //the slider is by default hidden
 
         ElenName.find(".Yes").on("click",function(){
@@ -12,7 +12,7 @@ function setupCheckbox(ElenName){
             //uncheck No box
             ElenName.find(".No").eq(0).prop("checked",false);
             //show the slider
-            ElenName.find(".aria-widget-slider").eq(1).slideDown(300)
+            SliderName.find(".aria-widget-slider").eq(1).slideDown(300)
         });
 
         ElenName.find(".No").on("click",function(){
@@ -20,6 +20,6 @@ function setupCheckbox(ElenName){
             //uncheck No box
             ElenName.find(".Yes").eq(0).prop("checked",false);
             //hide the slider
-            ElenName.find(".aria-widget-slider").eq(1).slideUp(300)
+            SliderName.find(".aria-widget-slider").eq(1).slideUp(300)
         });
 }
